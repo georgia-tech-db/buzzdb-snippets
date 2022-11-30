0d7786597d7de8dd775c5e0642ae4acc40ab7ba0
root <sencourant#>
2022-11-30 19:38:19 +0000

    add run-length encoding

--- src/include/utils/file.h
+++ src/include/utils/file.h
@@ -6,7 +6,17 @@
 namespace buzzdb{
 namespace utils{
 
-void hello_world();
+struct Element {
+    char c;
+    int offset;
+    int count;
+    Element(char c, int offset, int count): c(c), offset(offset), count(count){};
+    friend bool operator==(const Element& r1, const Element& r2) {
+        return r1.c == r2.c && r1.offset == r2.offset && r1.count == r2.count;
+    }
+};
+
+std::vector<Element> run_length_encoding(std::vector<char> data);
 
 }  // namespace utils
 }  // namespace buzzdb
--- test/unit/utils/file_test.cc
+++ test/unit/utils/file_test.cc
@@ -12,8 +12,36 @@
 
 namespace {
 
-TEST(FileTest, HelloWorld) {
-  buzzdb::utils::hello_world();
+TEST(FileTest, EncodingTest) {
+  std::string str = "wwwwaaadexxxxxxywww";
+  std::vector<char> input;
+  int n = str.length();
+  for (int i = 0; i < n; i++) {
+      input.push_back(static_cast<char>(str[i]));
+  }
+
+  auto result = buzzdb::utils::run_length_encoding(input);
+  /*
+  for (auto e: result) {
+    std::cout << "char: " << e.c << "offset: " << e.offset << "count: " << e.count << '\n';
+  }
+  */
+  std::vector<buzzdb::utils::Element> expected;
+  buzzdb::utils::Element e1 = {'w', 0, 4};
+  buzzdb::utils::Element e2 = {'a', 4, 3};
+  buzzdb::utils::Element e3 = {'d', 7, 1};
+  buzzdb::utils::Element e4 = {'e', 8, 1};
+  buzzdb::utils::Element e5 = {'x', 9, 6};
+  buzzdb::utils::Element e6 = {'y', 15, 1};
+  buzzdb::utils::Element e7 = {'w', 16, 3};
+  expected.push_back(e1);
+  expected.push_back(e2);
+  expected.push_back(e3);
+  expected.push_back(e4);
+  expected.push_back(e5);
+  expected.push_back(e6);
+  expected.push_back(e7);
+  EXPECT_EQ(expected, result);
 }
 
 }  // namespace
