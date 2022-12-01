314f32941447090ab58ee86c4f59835903103d12
TianaZhao <wzhao306@gatech.edu>
2022-12-01 19:24:50 +0000

    join done

--- src/utils/file.cc
+++ src/utils/file.cc
@@ -2,6 +2,11 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 
 namespace buzzdb {
 namespace utils {
@@ -10,6 +15,41 @@ namespace utils {
 
 void hello_world(){
   printf("Hello world");
+}
+
+vector<vector<std::string>> join(vector<vector<std::string>>& input_a, vector<vector<std::string>>& input_b, size_t col_a, size_t col_b) {
+  vector<vector<std::string>> result;
+  unordered_map<std::string, vector<vector<std::string>>> map_b;
+  for (size_t i = 0; i < input_b.size(); i++) {
+    auto key = input_b[i][col_b];
+    if (map_b.find(key) == map_b.end()) {
+      vector<vector<std::string>> added;
+      map_b[input_b[i][col_b]] = added;
+    }
+    map_b[key].push_back(input_b[i]);
+  }
+
+  for (size_t i = 0; i < input_a.size(); i++) {
+    auto key_a = input_a[i][col_a];
+    if (map_b.find(key_a) != map_b.end()) {
+      auto vector_b = map_b[key_a];
+      for (size_t j = 0; j< vector_b.size(); j++) {
+        vector<std::string> a_copy;
+        for (size_t z = 0; z < input_a[i].size(); z++) {
+          string a_str = input_a[i][z];
+          a_copy.push_back(a_str);
+        }
+
+        // copy(input_a[i].begin(), input_a[i].begin(), a_copy.begin());
+        for (size_t z = 0; z < vector_b[j].size(); z++) {
+          a_copy.push_back(vector_b[j][z]);
+        }
+        result.push_back(a_copy);
+      }
+    }
+  }
+  return result;
+
 }
 
 }  // namespace utils
--- test/unit/utils/file_test.cc
+++ test/unit/utils/file_test.cc
@@ -1,4 +1,3 @@
-
 #include <gtest/gtest.h>
 #include <algorithm>
 #include <cstdint>
@@ -6,14 +5,89 @@
 #include <exception>
 #include <random>
 #include <utility>
-#include <vector>
+#include <string>
 
 #include "utils/file.h"
+
+using namespace std;
 
 namespace {
 
 TEST(FileTest, HelloWorld) {
   buzzdb::utils::hello_world();
+}
+
+TEST(FileTest, BasicJoin) {
+std::vector<std::vector<std::string>> input_a{ { "Anna", "12" }, 
+                            { "Bella", "14" }, 
+                            { "Tom", "8" } 
+};
+
+std::vector<std::vector<std::string>> input_b{ { "Anna", "black" }, 
+                            { "Lily", "blue" }, 
+                            { "Bella", "yellow"} 
+};
+
+std::vector<std::vector<std::string>> ans = buzzdb::utils::join(input_a, input_b, 0, 0);
+for (size_t i = 0; i < ans.size(); i++) {
+    for (size_t j = 0; j < ans[i].size(); j++)
+        cout << ans[i][j] << " " << flush;
+    cout << endl << flush;
+}
+std::vector<std::vector<std::string>> expected_output = { { "Anna", "12", "Anna", "black" }, 
+                         { "Bella", "14", "Bella", "yellow" }
+};
+EXPECT_EQ(expected_output, ans);
+
+}
+
+TEST(FileTest, JoinWithDuplicateInTableB) {
+std::vector<std::vector<std::string>> input_a{ { "Anna", "12" }, 
+                            { "Bella", "14" }, 
+                            { "Tom", "8" } 
+};
+
+std::vector<std::vector<std::string>> input_b{ { "Anna", "black" }, 
+                            { "Anna", "blue" }, 
+                            { "Bella", "yellow"} 
+};
+
+std::vector<std::vector<std::string>> ans = buzzdb::utils::join(input_a, input_b, 0, 0);
+for (size_t i = 0; i < ans.size(); i++) {
+    for (size_t j = 0; j < ans[i].size(); j++)
+        cout << ans[i][j] << " " << flush;
+    cout << endl << flush;
+}
+std::vector<std::vector<std::string>> expected_output = { { "Anna", "12", "Anna", "black" }, 
+                         { "Anna", "12", "Anna", "blue" },
+                         { "Bella", "14", "Bella", "yellow" }
+};
+EXPECT_EQ(expected_output, ans);
+
+}
+
+TEST(FileTest, JoinWithBothDuplicates) {
+std::vector<std::vector<std::string>> input_a{ { "Anna", "12" }, 
+                            { "Anna", "14" }
+};
+
+std::vector<std::vector<std::string>> input_b{ { "Anna", "black" }, 
+                            { "Anna", "blue" }
+};
+
+std::vector<std::vector<std::string>> ans = buzzdb::utils::join(input_a, input_b, 0, 0);
+for (size_t i = 0; i < ans.size(); i++) {
+    for (size_t j = 0; j < ans[i].size(); j++)
+        cout << ans[i][j] << " " << flush;
+    cout << endl << flush;
+}
+std::vector<std::vector<std::string>> expected_output = { { "Anna", "12", "Anna", "black" }, 
+                         { "Anna", "12", "Anna", "blue" },
+                         { "Anna", "14", "Anna", "black" },
+                         { "Anna", "14", "Anna", "blue" }
+};
+EXPECT_EQ(expected_output, ans);
+
 }
 
 }  // namespace
