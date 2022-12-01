7e93b3247c38c06a83b6170b13f48d31fdbb3d9b
Varnika Budati <varnikabudati@gmail.com>
2022-12-01 18:57:14 -0500

    created a toy example for join and added basic tests

--- src/include/utils/file.h
+++ src/include/utils/file.h
@@ -2,11 +2,14 @@
 
 #include <cstdint>
 #include <memory>
+#include <vector>
 
 namespace buzzdb{
 namespace utils{
 
 void hello_world();
 
+std::vector<std::vector<int>> join(std::vector<std::vector<int>>& input1,
+    std::vector<std::vector<int>>& input2, size_t jk_idx_input1, size_t jk_idx_input2);
 }  // namespace utils
 }  // namespace buzzdb
